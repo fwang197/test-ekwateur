736c6c4d510ae82984f2ceb885b23c5541d475b9
Frederic Wang <frederic.wang@mymoneybank.com>
2024-03-25 17:59:04 +0100

    Fix dependance

--- src/main/java/com/ekwateur/test/calculator/Calculator.java
+++ src/main/java/com/ekwateur/test/calculator/Calculator.java
@@ -6,6 +6,14 @@ import com.ekwateur.test.models.Client;
 
 public sealed interface Calculator permits ParticulierCalculator, ProfessionnelCalculator {
 
+    /**
+     * Calcul le total en euro de la consommation du client via sa valeur KWH.
+     *
+     * @param client        particulier ou professionnel
+     * @param consommation  Gaz ou Electricité
+     * @param kwh           total kwh du consommateur
+     * @return
+     */
     double calculate(Client client, Consommation consommation, double kwh);
 
 }
